a676c31cb822be6de01ac803cd04133935f97eed
Katrina Swales <kat.swales@nekokittygames.com>
2015-05-07 18:39:20 +0200

    test more

--- src/test/java/com/nekokittygames/zmachine/tests/HeaderTests.java
+++ src/test/java/com/nekokittygames/zmachine/tests/HeaderTests.java
@@ -43,4 +43,14 @@ public class HeaderTests {
 
     }
 
+
+    @Test
+    public void MemorySetBit()
+    {
+        assertFalse("Should make a difference",0x00==Memory.setBit((byte)0x00,0,true));
+        assertEquals("Should make a correct difference", 0x1, Memory.setBit((byte) 0x00, 0, true));
+        assertEquals("Should make a correct difference with another bit", 0x4, Memory.setBit((byte) 0x00, 2, true));
+        assertEquals("Correctly handles retaining existing bit", 0x5, Memory.setBit((byte) 0x01, 2, true));
+    }
+
 }
