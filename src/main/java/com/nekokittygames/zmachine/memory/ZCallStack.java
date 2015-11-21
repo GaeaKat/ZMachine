7ebf682cc5b8279661d06236fef0b1752e79b59e
nekosune <kat.swales@nekokittygames.com>
2015-11-21 03:35:34 +0000

    Work on internals

--- src/main/java/com/nekokittygames/zmachine/memory/ZCallStack.java
+++ src/main/java/com/nekokittygames/zmachine/memory/ZCallStack.java
@@ -4,4 +4,29 @@ package com.nekokittygames.zmachine.memory;
  * Created by katsw on 20/11/2015.
  */
 public class ZCallStack {
+
+    public class ZCallStackElement
+    {
+        private final short framePointer;
+        private final ZFrame frame;
+        public ZCallStackElement(short framePointer,ZFrame frame)
+        {
+            this.framePointer=framePointer;
+            this.frame=frame;
+        }
+        public ZCallStackElement(ZFrame frame)
+        {
+            this((short)-1,frame);
+        }
+
+        public short getFramePointer() {
+            return framePointer;
+        }
+
+        public ZFrame getFrame() {
+            return frame;
+        }
+    }
+
+    
 }
